14b5ebf0a4a6b9c74c55d01c9ecfc514feb0940a
Utkarsh Shrivastava <utkarshshrivastava102002@gmail.com>
2025-10-29 00:33:12 +0530

    aunteticate complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
-import type { Metadata } from "next";
+import { type Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,12 +31,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Provider> {children}</Provider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Provider> {children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
